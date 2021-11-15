b246663765eb0dee42aa10a246b889ac223eb9b3
Cristiano Cunha <Contato@cristianoprogramador.com>
2021-11-15 10:24:07 -0300

    Implementa Controller de Transaction

---
+++ FakeBank.Api/Controllers/TransactionController.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Threading.Tasks;
+using FakeBank.Domain.Transactions.Commands;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace FakeBank.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TransactionController : Controller
+    {
+        private readonly ITransactionCommandHandler _handler;
+        private readonly ILogger<TransactionController> _logger;
+
+        public TransactionController(ITransactionCommandHandler handler, ILogger<TransactionController> logger)
+        {
+            _handler = handler;
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreteTransaction([FromBody] CreateTransactionCommand command)
+        {
+            try
+            {
+                await _handler.AddTransaction(command);
+                return NoContent();
+            }
+            catch (ArgumentException e)
+            {
+                return BadRequest(e.Message);
+            }
+            catch (Exception e)
+            {
+                //TODO: Implementar outras validações de erros
+                _logger.LogError("Ocorreu um erro inesperado", e);
+                return StatusCode(500);
+            }
+        }
+    }
+}
